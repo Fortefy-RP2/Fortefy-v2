225e358190bcc021358761dece4f23941fa985e8
Pedro <pnevesmartins@gmail.com>
2024-10-23 16:16:15 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'screens/aluno.dart';
+import 'screens/personal.dart';
+import 'screens/start.dart';
+//import 'screens/configuracoes_screen.dart';
+//import 'screens/chats_screen.dart';
+
+void main() {
+  runApp(FortefyApp());
+}
+
+final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+
+class FortefyApp extends StatelessWidget {
+  const FortefyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: _navigatorKey,
+      debugShowCheckedModeBanner: false,
+      title: 'Fortefy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //initialRoute: '/',
+      home: StartScreen(),
+      routes: {
+        '/start': (context) => StartScreen(),
+        '/aluno': (context) => AlunoScreen(),
+        '/personal': (context) => PersonalScreen(),
+      },
+    );
+  }
+}
+
